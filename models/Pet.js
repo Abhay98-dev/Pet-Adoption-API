20dd58a7b74c823634ca591bf854732b7ba7b56d
Abhay98-dev <chacharabhay@gmail.com>
2025-06-25 09:48:00 +0530

    added CRUD operations

--- models/Pet.js
+++ models/Pet.js
@@ -1,14 +1,20 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const petSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:true
+const petSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    type:{
-        type:String,
-        required:true
-    }
-})
-const Pet=mongoose.model("Pet",petSchema)
-module.exports=Pet
+    type: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }  
+);
+
+const Pet = mongoose.model("Pet", petSchema);
+module.exports = Pet;
+
--- routes/pet.js
+++ routes/pet.js
@@ -29,6 +29,24 @@ router.get("/",async(req,res)=>{
     }
 })
 
+router.get("/pets",async(req,res)=>{
+    const pet = req.query.type
+    if(pet.length===0){
+        return res.status(400).json({error:"Pet type is required"})
+    }
+    try{
+        const pets= await Pet.find({type:pet})
+        if(pets.length === 0){
+            return res.status(404).json({error:"No pets found of this type"})
+        }
+        return res.status(200).json(pets)
+    }
+    catch(err){
+        console.error("Error fetching pets by type: ",err)
+        res.status(500).json({error:"Internal Server Error"})
+    }
+})
+
 //get a pet by name
 router.get("/:name",async(req,res)=>{
     const PetName=req.params.name
@@ -44,4 +62,37 @@ router.get("/:name",async(req,res)=>{
         res.status(500).json({error:"Internal Server Error"})
     }
 })
+
+router.put("/:name",async(req,res)=>{
+    const PetName=req.params.name
+    const updateData=req.body
+    try{
+        const Update= await Pet.findOneAndUpdate({name:PetName},updateData,{new:true})
+        if(!Update){
+            return res.status(404).json({error:"Pet not found"})
+        }
+        return res.status(200).json(Update)
+    }
+    catch(err){
+        console.error("Error Updating pet:",err)
+        res.status(500).json({error:"Internal Server Error"})
+    }
+})
+
+router.delete("/:name",async(req,res)=>{
+    const PetName=req.params.name
+    try{
+        const deletedPet=await Pet.findOneAndDelete({name:PetName})
+        if(!deletedPet){
+            res.status(404).json({error:"Pet not found"})
+        }
+        return res.status(200).json({message:"Pet deleted successfully"})
+    }
+    catch(err){
+        console.error("Error deleting pet: ",err)
+        res.status(500).json({error:"Internal Server Error"})
+    }
+})
+
+
 module.exports=router
